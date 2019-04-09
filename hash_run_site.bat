2ac77121f1599846dd20afded20adf922259c274
k-doshi <36450592+k-doshi@users.noreply.github.com>
2019-04-09 11:31:30 -0500

    Update hash_run_site.bat

--- hash_run_site.bat
+++ hash_run_site.bat
@@ -21,17 +21,9 @@ echo begin hashing %TIME%
 :: HASHING BEGINS HERE	
 "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -E -b -S %Server% -d %Database% -s, -W -i ".\SQLFiles\InsertintoHash.sql" -o ".\Log\InsertLog_Temp.txt"
 if "%errorlevel%" == "1" goto err_handler
-if %encrypthhashfile%==1 goto next1_encrypt
 goto next1
 
-:: IF ENCRYPTION FLAG IS SET TO 1, PROCEED Hash csv WITH GENERATION ENCRYPTION
-:next1_encrypt
-echo begin creating csv for encryption %TIME%
-"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -E -b -S %Server% -d %Database%  -s, -W -i ".\SQLFiles\SelectHash.sql" | findstr /V /C:"-" /B > ".\Output\hashes_temp.csv"
-if "%errorlevel%" == "1" goto err_handler1
-goto success_encrypt
-
-:: IF ENCRYPTION FLAG IS SET TO 0 OR MISSING, PROCEED Hash csv WITHOUT ENCRYPTION
+:: PROCEED Hash and create csv
 :next1
 echo begin creating csv %TIME%
 "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd.exe" -E -b -S %Server% -d %Database%  -s, -W -i ".\SQLFiles\SelectHash.sql" | findstr /V /C:"-" /B > ".\Output\hashes_%siteid%_%projectid%_%d%.csv"
@@ -44,37 +36,13 @@ echo Insert failed with error #%errorlevel% >> ".\Log\InsertError_%d%.txt"
 SET /p delExit=Insert to sql table failed, review log files for more details. Press the ENTER key to exit...:
 exit /b
 
-:: ERROR DURING GENERATING CSV
+:: ERROR DURING GENERATING hash and CSV
 :err_handler1
 echo Export failed with error #%errorlevel% >> ".\Log\SelectError_%d%.txt"
 SET /p delExit=Export to csv failed, review log files for more details. Press the ENTER key to exit...:
 exit /b
 
-:: IF ENCRYPTION FLAG IS SET TO 1, COMPLETE LOG, ENCRYPT THE HASH FILE AND DELETE UNENCRYPTED HASH FILE
-:success_encrypt
-for /F "tokens=*"  %%i in ('type ".\Log\InsertLog_Temp.txt" ^| findstr /i "record"') do (
->> ".\Log\InsertCSVLog_%d%.txt" echo %%i)
-set count=0
-for /f "tokens=*" %%r in ('type ".\Log\InsertLog_Temp.txt" ^| findstr /i "row affected"') do (
-for /f "tokens=1 delims=^( " %%c in ("%%r") do set /a count=%%c
-)
-echo %count% records hashed, inserted into table, exported to csv, and encrypted successfully >> ".\Log\InsertCSVLog_%d%.txt"
-del ".\Log\InsertLog_Temp.txt"
-echo begin encryption %TIME%
-::begin hash file encryption
-::::create a random key to encrypt the hash output file
-"%~dp0DecryptSourceCodes\bin\openssl.exe" rand 64 -base64 -out ".\Output\encrypthashfilekey_temp.txt"
-::::encrypt the hash output file with key generated from above -AES
-"%~dp0DecryptSourceCodes\bin\openssl.exe" enc -aes-256-cbc -e -in ".\Output\hashes_temp.csv" -out ".\Output\enc_hashes_%siteid%_%projectid%_%d%.csv" -pass file:".\Output\encrypthashfilekey_temp.txt"
-::::encrypt the encrypthashfilekey with disambiguator's public key - RSA
-"%~dp0DecryptSourceCodes\bin\openssl.exe" rsautl -encrypt -pubin -inkey %disambiguatepublicKey% -in ".\Output\encrypthashfilekey_temp.txt" -out ".\Output\encHashkeyFile_%siteid%_%projectid%_%d%.txt"
-::::delete temporary hash file and plain text encryption key (encrypthashfilekey)
-del ".\Output\hashes_temp.csv"
-del ".\Output\encrypthashfilekey_temp.txt"
-SET /p delExit=Completed successfully, %TIME%. Press the ENTER key to exit...:
-exit /b
-
-:: IF ENCRYPTION FLAG IS SET TO 0 OR MISSING, COMPLETE LOG
+:: COMPLETE LOG
 :success
 for /F "tokens=*"  %%i in ('type ".\Log\InsertLog_Temp.txt" ^| findstr /i "record"') do (
 >> ".\Log\InsertCSVLog_%d%.txt" echo %%i)
